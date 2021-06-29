aadbf4ead4e28abd9e4e5b7d493b862dc1e9f3eb
shuibcodes <abdullahshuib124@gmail.com>
2021-06-29 23:08:41 +0100

    search input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Houston from './images/nba-logo-transparent.png'
+import NBA from './images/nba-logo-transparent.png'
+import OKC from './images/Oklahoma_City_Thunder.svg.png'
+import LAL from './images/1280px-Los_Angeles_Lakers_logo.svg.png'
+import PHX from './images/phoenix-suns-logo-black-and-white-transparent.png'
+import LAC from './images/1280px-The_official_logo_of_the_Los_Angeles_Clippers.svg.png'
+
+
+
 function App() {
   return (
     <div >
  <body class="bg-white-400" >
     
-    <div class="p-0  w-screen h-24 bg-red-700 flex py-4 justify-evenly   ">
+    <div class="p-0  w-screen h-24 bg-red-600 flex py-4 justify-evenly   ">
    
-      <h3 class="bg-blue-300 w-32  h-10 rounded px-8 py-1 " >Players</h3>
-      <h3 class=" w-46 bg-blue-300   h-10 rounded px-6 py-1">Eastern Conference</h3>
-      <h3 class=" w-46 bg-blue-300   h-10 rounded px-6 py-1" >Western Conference</h3>
+      <h3 class="bg-blue-400 w-32  h-10 rounded px-8 py-1 " >Players</h3>
+      <h3 class=" w-46 bg-blue-400   h-10 rounded px-6 py-1">Eastern Conference</h3>
+      <h3 class=" w-46 bg-blue-400   h-10 rounded px-6 py-1" >Western Conference</h3>
 
   
   
@@ -18,17 +25,25 @@ function App() {
 
 
 <div class="md:flex px-0">
-  <div class=" h-screen bg-red-700  w-48" >
-   
-    <img class="p-4 h-48 px-3 m-10" src={Houston} alt="" ></img>
+  <div class=" h-screen bg-red-600  w-48" >
+  <form class=" relative top-16 px-3 rounded " action="/action_page.php" >
+  <input class="h-10" type="text" id="fname" name="fname" placeholder="Search Team" />
+
+</form>
+    <img class="relative bottom-36  h-24 px-3 mx-10 mt-4 " src={NBA} alt="" ></img>
+    <img class=" h-36 px-3 m-0 " src={PHX} alt="" ></img>
+    <img class=" h-24  m-10 " src={OKC} alt="" ></img>
+    <img class=" h-24 px-3 m-0 " src={LAL} alt="" ></img>
+    <img class=" h-24 px-4 my-10 " src={LAC} alt="" ></img>
+
+
   </div>
    <div class="w-48 mx-20 py-24 ">
      <div>
-      <p>Lorem, ipsum dolorefe sit amet consectetur adipisicing elit. Numquam quia eum quo quod incidunt, earum expedita ut ullam! Consectetur nesciunt magnam tempora ab commodi facere, ipsum deleniti soluta minus reiciendis!</p>
+     
      </div>
     
-     <img class="p-4 h-48 px-3 m-10" src={Houston} alt="" ></img>
-
+ 
    </div>
 </div>
 
