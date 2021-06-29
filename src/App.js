a2893548f23733edf18374279b03fca0bb0d4559
shuibcodes <abdullahshuib124@gmail.com>
2021-06-29 21:37:00 +0100

    tailwind layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Houston from './images/nba-logo-transparent.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+ <body class="bg-white-400" >
+    
+    <div class="p-0  w-screen h-24 bg-red-700 flex py-4 justify-evenly   ">
+   
+      <h3 class="bg-blue-300 w-32  h-10 rounded px-8 py-1 " >Players</h3>
+      <h3 class=" w-46 bg-blue-300   h-10 rounded px-6 py-1">Eastern Conference</h3>
+      <h3 class=" w-46 bg-blue-300   h-10 rounded px-6 py-1" >Western Conference</h3>
+
+  
+  
+    </div>
+
+
+<div class="md:flex px-0">
+  <div class=" h-screen bg-red-700  w-48" >
+   
+    <img class="p-4 h-48 px-3 m-10" src={Houston} alt="" ></img>
+  </div>
+   <div class="w-48 mx-20 py-24 ">
+     <div>
+      <p>Lorem, ipsum dolorefe sit amet consectetur adipisicing elit. Numquam quia eum quo quod incidunt, earum expedita ut ullam! Consectetur nesciunt magnam tempora ab commodi facere, ipsum deleniti soluta minus reiciendis!</p>
+     </div>
+    
+     <img class="p-4 h-48 px-3 m-10" src={Houston} alt="" ></img>
+
+   </div>
+</div>
+
+
+
+ 
+</body>
     </div>
   );
 }
