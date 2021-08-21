6f57ddeef5dc381c9174220eb20c756cbe9621f8
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2021-08-21 17:56:33 +0100

    finished MPV line chart

--- src/Charts/RoadToFinals.js
+++ src/Charts/RoadToFinals.js
@@ -31,7 +31,7 @@ import CurryStats from '../Stats/CurryStats'
   
   ]
   
-  const russ = [
+  const westbrook = [
     {
       "season": 2016,
       "pts":31.6,
@@ -45,7 +45,7 @@ import CurryStats from '../Stats/CurryStats'
       "pts":22.9,
     },
     {
-      "season": 27.2,
+      "season": 2019,
       "pts":25.3,
     },
     {
@@ -111,33 +111,144 @@ import CurryStats from '../Stats/CurryStats'
 function RoadToFinals() {
 
     
-  const data = useMemo(
-    () => [
-      {
-          "id": "Booker",
-          "color": "hsl(232, 70%, 50%)",
-          "data": [
+ 
         
-           
-          ]
-        }
-      ],
   
-    []
-  );
+  const durantSet=  [ {
+    "id": "K Durant",
+    "color": "hsl(22, 70%, 50%)",
+    "data": [
+     
+      
+    ]
+  }
+  ]
 
-
+    
+  const LebronSet=  [ {
+    "id": "L James",
+    "color": "hsl(22, 70%, 50%)",
+    "data": [
+     
+      
+    ]
+  }
+  ]
+  const CurrySet=  [ {
+    "id": "S Curry",
+    "color": "hsl(22, 70%, 50%)",
+    "data": [
+     
+      
+    ]
+  }
+  ]
+  const RussSet=  [ {
+    "id": "R Westbrook",
+    "color": "hsl(22, 70%, 50%)",
+    "data": [
+     
+      
+    ]
+  }
+  ]
+  
+    const filteredData = (data, nbaData) => {
+      const newData = data[0].data.push(...nbaData)
+      console.log(data)
+      const fd = data[0].data
+      console.log(data)
+      const newFd = fd.map((e) => Object.keys(fd[0])) 
+      console.log(fd)
+      // replacing with x and y 
+      var i;
+      for(i = 0; i < fd.length; i++){
+          fd[i].x = fd[i]['season'];
+          fd[i].y = fd[i]['pts'];
+         
+    }
+    console.log(data)
+    return data
+    
+    }
+  
+  
+  
+    
+  const kevin = filteredData(durantSet,kd)
+  const russ = filteredData(RussSet,westbrook)
+  const steph= filteredData(CurrySet,curry)
+  const james= filteredData(LebronSet,lebron)
+  
+  russ.push(kevin[0])
+  russ.push(steph[0])
+  russ.push(james[0])
   
   
 
     return (
 
+      <div  style={{height:"350px", width:"900px", padding:"0px", position:"relative", right:"20px"}} >
+      <ResponsiveLine
+ data={russ}
+ margin={{ top: 50, right: 110, bottom: 50, left: 60 }}  
 
-      
-        <div>
-
-
-        </div>
+ xScale={{ type: 'point' }}
+ yScale={{ type: 'linear', min: '10', max: '130', stacked: true, reverse: false }}
+ axisTop={null}
+ axisRight={null}
+ axisBottom={{
+     orient: 'bottom',
+     tickSize: 5,
+     tickPadding: 5,
+     tickRotation: 0,
+     legend: 'seasons',
+     legendOffset: 36,
+     legendPosition: 'middle'
+ }}
+ axisLeft={{
+     orient: 'left',
+     tickSize: 5,
+     tickPadding: 5,
+     tickRotation: 0,
+     legend: 'points',
+     legendOffset: -40,
+     legendPosition: 'middle'
+ }}
+ pointSize={10}
+ pointColor={{ theme: 'background' }}
+ pointBorderWidth={2}
+ pointBorderColor={{ from: 'serieColor' }}
+ pointLabelYOffset={-12}
+ useMesh={true}
+ legends={[
+     {
+         anchor: 'right',
+         direction: 'column',
+         justify: false,
+         translateX: 105,
+         translateY: -10,
+         itemsSpacing: 0,
+         itemDirection: 'left-to-right',
+         itemWidth: 80,
+         itemHeight: 20,
+         itemOpacity: 0.75,
+         symbolSize: 12,
+         symbolShape: 'circle',
+         symbolBorderColor: 'rgba(0, 0, 0, .5)',
+         effects: [
+             {
+                 on: 'hover',
+                 style: {
+                     itemBackground: 'rgba(0, 0, 0, .03)',
+                     itemOpacity: 1
+                 }
+             }
+         ]
+     }
+ ]}
+/>
+ </div>
     )
 }
 
